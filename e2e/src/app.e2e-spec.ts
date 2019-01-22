9cdf4c3a138dc159a2ab81852eca1da4545d2c59
Simon Varvat <simon.varvat@gmail.com>
2019-01-22 14:02:51 +0100

    blbl test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,10 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
+<<<<<<< hop
     expect(page.getTitleText()).toEqual('Welcome to test-test!');
+=======
+    expect(page.getTitleText()).toEqual('Welcome to test-git!');
+>>>>>>> blbl test
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< hop
   title = 'test-test';
+=======
+  title = 'test-git';
+>>>>>>> blbl test
 }
